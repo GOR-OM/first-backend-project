2f85e23dc2082c4e48822026182efb6a378a68b5
GOR-OM <202101484@daiict.ac.in>
2023-12-19 04:35:41 +0530

    created models

---
+++ models/task.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+
+
+const userSchema = new mongoose.Schema({
+    name : String,
+    title : {
+        type : String,
+        required : true,
+    },
+    description :{
+        type : String,
+        required : true,
+    },
+    isCompleted : {
+        type : Boolean,
+        default : false,
+    },
+    user :{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true,
+    },
+
+    
+});
+
+export const task = mongoose.model("task", userSchema);
